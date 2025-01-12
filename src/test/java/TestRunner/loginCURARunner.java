9df7439aea72e12de53f96d2a79abffff166a331
Aditia Angga Perdana <aditia.anggaperdana@gmail.com>
2025-01-12 09:47:32 +0700

    add LoginCURARunner

---
+++ src/test/java/TestRunner/loginCURARunner.java
@@ -0,0 +1,14 @@
+package TestRunner;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="src/test/resources/Features/loginCURA.feature",
+        glue= {"StepDef"},
+        plugin ={"pretty","json:target/cucumber-result/json/cucumber.json"
+                , "html:target/cucumber-result/html/cucumber.html"
+}
+        )
+public class loginCURARunner {
+}
