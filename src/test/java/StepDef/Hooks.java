552ef5c3d2c13b4be5b5875b3ea0505be0d2fcd1
aditiaangga <aditia.anggaperdana@gmail.com>
2025-03-01 15:27:47 +0700

    Update Hooks.java

--- src/test/java/StepDef/Hooks.java
+++ src/test/java/StepDef/Hooks.java
@@ -14,6 +14,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.safari.SafariOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import utils.DriverManager;
 
 import javax.imageio.ImageIO;
@@ -21,6 +22,8 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.net.URL;
+import java.net.MalformedURLException;
 
 public class Hooks {
     WebDriver driver;
