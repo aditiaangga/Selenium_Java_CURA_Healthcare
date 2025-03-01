0bd4697c8ffc547885661a2af82b281a9ffa34bb
Aditia Angga Perdana <aditia.anggaperdana@gmail.com>
2025-03-01 07:17:30 +0700

    update calendar

--- src/test/java/pages/Homepage.java
+++ src/test/java/pages/Homepage.java
@@ -95,7 +95,7 @@ public class Homepage {
         int year = targetDate.getYear();
         int day = targetDate.getDayOfMonth();
 
-        driver.findElement(visitDate).click();
+        waitForElementVisible(visitDate,10).click();
         waitForElementVisible(By.xpath("(//th[@class='datepicker-switch'])[1]"),10).click();
         waitForElementVisible(By.xpath("(//th[@class='datepicker-switch'])[2]"),10).click();
         waitForElementVisible(By.xpath("//span[.='"+year+"']"),10).click();
