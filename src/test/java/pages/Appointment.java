3581f22e41a4474da1703e9b093f3b690ab1bec6
aditiaangga <aditia.anggaperdana@gmail.com>
2025-03-13 06:00:34 +0700

    Update Appointment.java

--- src/test/java/pages/Appointment.java
+++ src/test/java/pages/Appointment.java
@@ -4,6 +4,10 @@ import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class Appointment {
     WebDriver driver;
@@ -14,9 +18,11 @@ public class Appointment {
     }
 
     public void verifyAppointment(){
-        WebElement confirmation = driver.findElement(By.id("summary"));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        WebElement confirmation = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("summary")));
         confirmation.isDisplayed();
         System.out.println(confirmation.getText());
+        //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 //        Assertions.assertEquals("Appointment Confirmation", confirmation.getText());
     }
 }
